4aecb3aff9b712ea3945d5019b103dee2bde9de6
Dave Sag <davesag@gmail.com>
2020-07-15 07:02:11 +1000

    #17 better selectors for when poster shared a link

--- tests/content/facebook/utils/post/pre2020/getPosterLink.test.js
+++ tests/content/facebook/utils/post/pre2020/getPosterLink.test.js
@@ -1,19 +1,56 @@
-import getPosterLink, { POSTER } from 'content/facebook/utils/post/pre2020/getPosterLink';
+import getPosterLink, {
+  POSTER_SHARED_A_LINK,
+  POSTER_NO_SHARE
+} from 'content/facebook/utils/post/pre2020/getPosterLink';
 
 const querySelector = jest.fn();
 
 const expected = 'this yay!';
 let result;
 
-beforeAll(() => {
-  querySelector.mockReturnValue(expected);
-  result = getPosterLink({ querySelector });
+const cleanup = () => {
+  querySelector.mockClear();
+  result = undefined;
+};
+
+describe('when poster shared a link', () => {
+  beforeAll(() => {
+    querySelector.mockReturnValue(expected);
+    result = getPosterLink({ querySelector });
+  });
+
+  afterAll(cleanup);
+
+  it('called querySelector with POSTER_SHARED_A_LINK', () => {
+    expect(querySelector).toHaveBeenCalledWith(POSTER_SHARED_A_LINK);
+  });
+
+  it('did not call querySelector with POSTER_NO_SHARE', () => {
+    expect(querySelector).not.toHaveBeenCalledWith(POSTER_NO_SHARE);
+  });
+
+  it('returned the expected result', () => {
+    expect(result).toEqual(expected);
+  });
 });
 
-it('called querySelector with POSTER', () => {
-  expect(querySelector).toHaveBeenCalledWith(POSTER);
-});
+describe('when poster did not share a link', () => {
+  beforeAll(() => {
+    querySelector.mockReturnValueOnce(null).mockReturnValueOnce(expected);
+    result = getPosterLink({ querySelector });
+  });
 
-it('returned the expected result', () => {
-  expect(result).toEqual(expected);
+  afterAll(cleanup);
+
+  it('called querySelector with POSTER_SHARED_A_LINK', () => {
+    expect(querySelector).toHaveBeenCalledWith(POSTER_SHARED_A_LINK);
+  });
+
+  it('called querySelector with POSTER_NO_SHARE', () => {
+    expect(querySelector).toHaveBeenCalledWith(POSTER_NO_SHARE);
+  });
+
+  it('returned the expected result', () => {
+    expect(result).toEqual(expected);
+  });
 });
