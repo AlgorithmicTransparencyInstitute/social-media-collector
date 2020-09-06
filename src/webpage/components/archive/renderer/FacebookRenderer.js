80583e21fd26d8554128d58442cff244e605d8a2
Alex Guo <chessnut@outlook.com>
2020-09-05 17:45:27 -0700

    Issue: Archive has poor display issues for post2020 ads.
    Fix: a custom parser to extract the ad copy and images. Then write our
    own html to display the contents.

--- src/webpage/components/archive/renderer/FacebookRenderer.js
+++ src/webpage/components/archive/renderer/FacebookRenderer.js
@@ -39,6 +39,117 @@ AdTargeting.propTypes = {
   item: PropTypes.shape(itemShape).isRequired
 };
 
+// Find the ad image used. The tricky part here is there could be two different
+// kinds of ways to parse (I think based on pre and post2020).
+function getAdImg(doc /* DOMParser */) {
+  // Pre 2020. Much easier to parse.
+  var imgs = doc.querySelectorAll('div.fbStoryAttachmentImage');
+  if (imgs.length > 0) {
+    return imgs[0];
+  }
+
+  // Post 2020
+  imgs = doc.querySelectorAll('img');
+  for (var i = 0; i < imgs.length; ++i) {
+    var img = imgs[i];
+
+    // These are the conditions where we know an image is not an ad image.
+    // 1) `src` contains `data:image/svg+xml` -- an example is a heart icon
+    // 2) Contains `/emoji.php/` -- these are emojis within the ad copy.
+    // 3) Contains `/images/video/play` -- Video play icon.
+    // 4) Width, height is less than 75px -- could be profile pic.
+    // 5) Has `aria-label` -- this is usually advertiser name like `ManyChat`
+    // 6) `src` contains '/rsrc.php/' -- Seems to be FB pixel.
+    var src = img.getAttribute('src');
+    var h = parseInt(img.getAttribute('height'), 10) || 76;
+    var w = parseInt(img.getAttribute('width'), 10) || 76;
+    var arialabel = img.getAttribute('aria-label');
+    if (
+      src.indexOf('data:image/svg+xml') >= 0 ||
+      src.indexOf('/emoji.php/') >= 0 ||
+      src.indexOf('/images/video/play') >= 0 ||
+      w < 75 ||
+      h < 75 ||
+      arialabel != null ||
+      src.indexOf('/rsrc.php/') >= 0
+    ) {
+      continue;
+    }
+    return img;
+  }
+}
+
+function newDoc(html /* string */) {
+  // From https://stackoverflow.com/a/30040354/3325787
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(html, 'text/html');
+  return doc;
+}
+
+function getAdCopyPre2020(doc /* DOMParser */) {
+  var html = '';
+  var els = doc.querySelectorAll('div.userContent');
+  if (els.length > 0) {
+    for (var i = 0; i < els.length; ++i) {
+      html += els[i].innerHTML;
+    }
+  }
+  return html;
+}
+
+function getAdCopyPost2020(doc /* DOMParser */) {
+  var html = '';
+  var els = doc.querySelectorAll('div[dir="auto"], span[dir="auto"]');
+  for (var i = 0; i < els.length; ++i) {
+    var el = els[i];
+    if (el.childElementCount > 1) {
+      continue;
+    }
+    // This 'el' could still be valid it has a child element for the reason of
+    // an emoji.
+    if (el.childElementCount === 1 && el.innerHTML.indexOf('/emoji.php') >= 0) {
+      html += el.innerHTML;
+    } else if (el.childElementCount === 0) {
+      html += el.innerText + '<br />';
+    }
+  }
+  return html;
+}
+
+function getAdCopy(doc /* DOMParser */) {
+  var html = getAdCopyPre2020(doc);
+  if (html.length > 0) {
+    return html;
+  }
+  return getAdCopyPost2020(doc);
+}
+
+function getAdvertiser(doc /* DOMParser */) {
+  // pre2020
+  var els = doc.querySelectorAll('a[data-hovercard]');
+  if (els.length > 1) {
+    return els[1].outerHTML;
+  }
+
+  // post2020
+  els = doc.querySelectorAll('h4[dir="auto"]');
+  if (els.length === 0) {
+    return '';
+  }
+  return els[0].innerHTML;
+}
+
+function makeAdHtml(html /* string */) {
+  var doc = newDoc(html);
+  var img = getAdImg(doc);
+  if (!img) {
+    return html;
+  }
+  return (
+    getAdvertiser(doc) + getAdCopy(doc) + '<img src="' + img.getAttribute('src') + '" width=500>'
+  );
+}
+
 const FacebookRenderer = ({ item }) => {
   const {
     id,
@@ -49,9 +160,11 @@ const FacebookRenderer = ({ item }) => {
 
   const classes = `ati-item fb-item fb-${itemType}`;
 
+  var adhtml = makeAdHtml(html);
+
   return (
     <div className="archive-item">
-      <div id={id} className={classes} dangerouslySetInnerHTML={{ __html: html }} />
+      <div id={id} className={classes} dangerouslySetInnerHTML={{ __html: adhtml }} />
       <AdTargeting item={item} />
     </div>
   );
