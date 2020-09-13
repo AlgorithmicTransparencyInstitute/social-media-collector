8453c7330e93740ffdcc5a4fd7e1a79ca8478593
Jeremy B Merrill <jeremybmerrill@gmail.com>
2020-09-13 14:45:31 -0400

    rearrange obfuscated post2020 sponsored tag detector

--- src/content/facebook/utils/post/post2020/isSponsoredPost.js
+++ src/content/facebook/utils/post/post2020/isSponsoredPost.js
@@ -9,13 +9,22 @@ const LINK = ':scope a[role="link"],:scope div[role="button"],:scope div[aria-la
  *  @param {String} sponsorStr - Sponsored string to scan for. (Defaults to 'Sponsored').
  *  @returns {Boolean} true if the post subtitle contains a visible sponsored substring.
  */
+
+// TODO: in some cases, I suspect, FB may switch to having obfuscated sponsor labels that
+//       actually manage to say "Sponsored" to a screenreader by using the aria-labelledby
+//       attribute, pointing to an element elsewhere in the DOM tree (not in the ad element)
+//       that contains the unobfuscated text "Sponsored." So, we should account for that
+//       somehow, perhaps by finding all elements with a labelledby attr, seeing if they
+//       point to something that says "Sponsored" and, if so, considering anything that
+//       is labeled by that element to be an ad.
 const isSponsoredPost = (element, sponsorStr = 'Sponsored') =>
   Boolean(
     Array.from(element.querySelectorAll(LINK)).find(link => {
-      if (link.getAttribute('aria-label') === sponsorStr) {
-        return true;
-      }
-      return link.innerText === sponsorStr || link.innerText.indexOf(sponsorStr) === 0;
+      return (
+        link.innerText === sponsorStr || // ordinary ads
+        link.innerText.indexOf(sponsorStr) === 0 || // political ads that say, e.g. Sponsored • Paid for by Whoever
+        link.getAttribute('aria-label') === sponsorStr // obfuscated ads that say, e.g. tiSSpponeonssetormoreidealagd but have a nearby div with aria-label="Sponsored"
+      );
     })
   );
 
