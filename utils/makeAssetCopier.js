7dd1648c167ae32ad17bdf90aa660e160a0a8fef
Jeremy B Merrill <jeremybmerrill@gmail.com>
2020-10-27 14:42:01 -0400

    updates deps

--- utils/makeAssetCopier.js
+++ utils/makeAssetCopier.js
@@ -2,11 +2,13 @@ const { resolve } = require('path');
 const Copy = require('copy-webpack-plugin');
 
 const makeAssetCopier = (buildFolder, buildKey, assetsPath) => (source, target) =>
-  new Copy([
-    {
-      from: `${source}/${assetsPath}/`,
-      to: resolve(__dirname, '..', buildFolder, buildKey, target || source, 'assets')
-    }
-  ]);
+  new Copy({
+    patterns: [
+      {
+        from: `${source}/${assetsPath}/`,
+        to: resolve(__dirname, '..', buildFolder, buildKey, target || source, 'assets')
+      }
+    ]
+  });
 
 module.exports = makeAssetCopier;
