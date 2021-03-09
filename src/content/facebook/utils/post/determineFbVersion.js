6cd1424c7e18051f94fbf972bcf0abd1f8c45ba5
Jeremy B Merrill <jeremybmerrill@gmail.com>
2021-03-09 09:57:21 -0500

    actually vx.0.12; if pre2020 detected, set elemetns to empty list, to do nothing

--- src/content/facebook/utils/post/determineFbVersion.js
+++ src/content/facebook/utils/post/determineFbVersion.js
@@ -1,13 +1,23 @@
 import isElementInViewport from '../visible/isElementInViewport';
 import notOverlapping from '../visible/notOverlapping';
 
+import { HYPERFEED } from '../../constants';
+
+const DIVS_WITH_IDS = 'div[id]';
 const DIV_HAS_ROLE_IS_ARTICLE = 'div [role="article"]';
 
+const isHyperfeed = element => element.getAttribute('id').startsWith(HYPERFEED);
+
 const determineFbVersion = (/* istanbul ignore next */ doc = document) => {
-  const elements = Array.from(doc.querySelectorAll(DIV_HAS_ROLE_IS_ARTICLE));
+  const pre2020 = Array.from(doc.querySelectorAll(DIVS_WITH_IDS)).filter(isHyperfeed);
+
+  const [version, elements] = pre2020.length
+    ? ['pre2020', []] // if pre2020, return no elements at all, we don't want to do ANYTHING.
+    : ['post2020', Array.from(doc.querySelectorAll(DIV_HAS_ROLE_IS_ARTICLE))];
+
   return {
     elements: elements.filter(isElementInViewport).reduce(notOverlapping, []),
-    version: 'post2020'
+    version
   };
 };
 
